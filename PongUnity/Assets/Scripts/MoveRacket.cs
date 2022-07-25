8bd54030771414778509fc6e60fcee40fbbd53d0
CAIIU4iNO <caiiu4ino@gmail.com>
2022-07-25 15:43:13 +0300

    add Script Racket

---
+++ PongUnity/Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    public float speed = 30;
+    public string axis = "Vertical";
+
+    private void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
