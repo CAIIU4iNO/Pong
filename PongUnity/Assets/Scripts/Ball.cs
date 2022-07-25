2fb5236bf487eb875955c80b28034e6f915a0413
CAIIU4iNO <caiiu4ino@gmail.com>
2022-07-25 16:15:25 +0300

    update Ball script

--- PongUnity/Assets/Scripts/Ball.cs
+++ PongUnity/Assets/Scripts/Ball.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class Ball : MonoBehaviour
 {
@@ -8,5 +10,52 @@ public class Ball : MonoBehaviour
     {
         //Initial Velocity
         GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        // Note: 'col' holds the collision information. If the
+        // Ball collided with a racket, then:
+        //   col.gameObject is the racket
+        //   col.transform.position is the racket's position
+        //   col.collider is the racket's collider
+
+        // Hit the left Raclet?
+        if(col.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            // Calculate direction? make Length = 1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Raclet?
+        if (col.gameObject.name == "Racketright")
+        {
+            // Calculate hit factor
+            float y = hitFactor(transform.position, col.transform.position, col.collider.bounds.size.y);
+
+            // Calculate direction? make Length = 1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+    }
+
+    private float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHight;
     }
 }
